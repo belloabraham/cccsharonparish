87bb8d347e1d5ab66917a5596c35087393b573b5
Bello Abraham <belloabraham101@gmail.com>
2023-10-06 23:29:18 +0100

    Updated project content security policy

--- apps/admin.mydailydigest.me/src/main.ts
+++ apps/admin.mydailydigest.me/src/main.ts
@@ -1,5 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import * as DOMPurify from 'dompurify';
+
+if (window.trustedTypes && window.trustedTypes.createPolicy) {
+  window.trustedTypes.createPolicy('default', {
+    createHTML: (string, sink) => DOMPurify.sanitize(string),
+  });
+}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
