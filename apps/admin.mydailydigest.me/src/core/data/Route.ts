794d7803ac97e97d13251d610b6b27e5cd514164
Bello Abraham <belloabraham101@gmail.com>
2024-02-18 15:01:28 +0100

    WIP

--- apps/admin.mydailydigest.me/src/core/data/Route.ts
+++ apps/admin.mydailydigest.me/src/core/data/Route.ts
@@ -1,3 +1,4 @@
 export class Route {
   static readonly ROOT = '';
+  static readonly VERIFY_EMAIL = 'verify-email';
 }
--- apps/admin.mydailydigest.me/src/main.ts
+++ apps/admin.mydailydigest.me/src/main.ts
@@ -1,13 +1,17 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import * as DOMPurify from 'dompurify';
+import DOMPurify from 'dompurify';
 
 if (window.trustedTypes && window.trustedTypes.createPolicy) {
   window.trustedTypes.createPolicy('default', {
-    createHTML: (string, sink) => DOMPurify.sanitize(string),
+    createHTML: (string, sink) => {
+      return DOMPurify.sanitize(string, { RETURN_TRUSTED_TYPE: true }) as any;
+    },
+    //Permits src to be set for script url by non angular code
+    //The browser 'default' CSP policy approves that this url is safe irrespective of any url
+    createScriptURL: (string) => string,
   });
 }
-
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
