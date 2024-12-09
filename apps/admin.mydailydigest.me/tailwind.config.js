b0c3822fea161c5e32d3c06cd6ce7b99a13eb92f
Bello Abraham <belloabraham101@gmail.com>
2024-12-09 15:38:59 +0100

    Added tailwind

---
+++ apps/admin.mydailydigest.me/tailwind.config.js
@@ -0,0 +1,24 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    screens: {
+      xs:'320px',
+      sm: '425px',
+      md: '576px',
+      tb: '768px',
+      lg: '976px',
+      xl: '1200px',
+      '2xl': '1440px',
+      '3xl': '2560px',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
