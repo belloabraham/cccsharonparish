637566063af54f3fe6db5e492110ab0f649d986c
Bello Abraham <belloabraham101@gmail.com>
2024-12-27 07:01:02 +0100

    WIP

--- apps/admin.mydailydigest.me/src/app/dashboard/dashboard.component.ts
+++ apps/admin.mydailydigest.me/src/app/dashboard/dashboard.component.ts
@@ -4,9 +4,10 @@ import { TuiNavigation } from '@taiga-ui/layout';
 import { CommonComponent, PAGE_TITLE_KEY } from '../shared';
 import { DASHBOARD_STRING_RESOURCE_KEY } from './i18n/string-res-keys';
 import { DashboardService } from './dashboard.service';
-import { AsyncPipe, NgIf } from '@angular/common';
+import { AsyncPipe, NgIf, NgOptimizedImage } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-dashboard',
@@ -17,6 +18,7 @@ import { MatIconModule } from '@angular/material/icon';
     NgIf,
     MatButtonModule,
     MatIconModule,
+    NgOptimizedImage,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
@@ -30,6 +32,7 @@ import { MatIconModule } from '@angular/material/icon';
 export class DashboardComponent extends CommonComponent {
   readonly dashboardService = inject(DashboardService);
   minimizeSideNav = signal(false);
+  appName = environment.appName
 
   toggleTheme() {
     this.dashboardService.toggleTheme();
