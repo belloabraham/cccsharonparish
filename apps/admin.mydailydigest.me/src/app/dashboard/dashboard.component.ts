91476bffcf7bfc3f6ea09ead543af7fe10c618e7
Bello Abraham <belloabraham101@gmail.com>
2024-12-31 19:03:39 +0100

    WIP

--- apps/admin.mydailydigest.me/src/app/dashboard/dashboard.component.ts
+++ apps/admin.mydailydigest.me/src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,20 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { TuiNavigation } from '@taiga-ui/layout';
+import {
+  TuiNavigation,
+} from '@taiga-ui/layout';
 import { CommonComponent, PAGE_TITLE_KEY, SharedModule } from '../shared';
 import { DASHBOARD_STRING_RESOURCE_KEY } from './i18n/string-res-keys';
 import { DashboardService } from './dashboard.service';
-import { AsyncPipe, NgIf, NgOptimizedImage } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf, NgOptimizedImage } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { environment } from '../../environments/environment';
-import { TuiDrawer, TuiChevron } from '@taiga-ui/kit';
-import { TuiPopup } from '@taiga-ui/core';
+import { TuiDrawer, TuiChevron, TuiBreadcrumbs, TuiFade } from '@taiga-ui/kit';
+import { TuiLink, TuiPopup } from '@taiga-ui/core';
 import { MatRippleModule } from '@angular/material/core';
 import { ROUTE } from '@cccsharonparish/mydailydigest';
+import { TuiItem, TuiRepeatTimes } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'app-dashboard',
@@ -30,6 +33,12 @@ import { ROUTE } from '@cccsharonparish/mydailydigest';
     SharedModule,
     TuiChevron,
     RouterLink,
+    TuiBreadcrumbs,
+    NgFor,
+    NgIf,
+    TuiFade,
+    TuiLink,
+    TuiItem
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
@@ -47,6 +56,12 @@ export class DashboardComponent extends CommonComponent {
   readonly openSideDrawer = signal(false);
   KEY = DASHBOARD_STRING_RESOURCE_KEY;
   ROUTE = ROUTE;
+  protected readonly breadcrumbs = [
+    'Home',
+    'Angular',
+    'Repositories',
+    'Taiga UI',
+  ];
 
   onClose() {
     this.openSideDrawer.set(false);
