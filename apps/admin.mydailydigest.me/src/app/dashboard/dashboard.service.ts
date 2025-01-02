f211bebe7656be460fa2000d0f0bab213717f357
Bello Abraham <belloabraham101@gmail.com>
2025-01-02 17:00:10 +0100

    WIP

--- apps/admin.mydailydigest.me/src/app/dashboard/dashboard.service.ts
+++ apps/admin.mydailydigest.me/src/app/dashboard/dashboard.service.ts
@@ -3,13 +3,13 @@ import { ThemeService, ThemeType } from '@cccsharonparish/angular';
 import { TuiBreakpointService } from '@taiga-ui/core';
 import { environment } from '../../environments/environment';
 import { Settings } from '@cccsharonparish/mydailydigest';
-import { AuthService } from '../services';
+import { AUTH_TOKEN, AuthService } from '../services';
 
 @Injectable()
 export class DashboardService {
   readonly breakpoint$ = inject(TuiBreakpointService);
   private readonly themeService = inject(ThemeService);
-  private readonly authService = inject(AuthService);
+  private readonly authService = inject(AUTH_TOKEN);
 
   logout() {
     return this.authService.signOut();
--- apps/admin.mydailydigest.me/src/app/dashboard/profile/profile.component.ts
+++ apps/admin.mydailydigest.me/src/app/dashboard/profile/profile.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {
-
-}
+export class ProfileComponent {}
