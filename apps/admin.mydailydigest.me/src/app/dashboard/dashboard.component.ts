ed1e6fb0d1adb6483050d8483405fcedfc5c8ea4
Bello Abraham <belloabraham101@gmail.com>
2025-01-04 22:39:31 +0100

    WIP

--- apps/admin.mydailydigest.me/src/app/dashboard/dashboard.component.ts
+++ apps/admin.mydailydigest.me/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, effect, inject, signal } from '@angular/core';
 import {
   ActivatedRoute,
   NavigationEnd,
+  NavigationStart,
   Router,
   RouterLink,
   RouterLinkActive,
@@ -11,7 +12,13 @@ import { TuiNavigation } from '@taiga-ui/layout';
 import { CommonComponent, PAGE_TITLE_KEY, SharedModule } from '../shared';
 import { DASHBOARD_STRING_RESOURCE_KEY } from './i18n/string-res-keys';
 import { DashboardService, IBreadCrumb } from './dashboard.service';
-import { AsyncPipe, NgFor, NgIf, NgOptimizedImage } from '@angular/common';
+import {
+  AsyncPipe,
+  NgFor,
+  NgIf,
+  NgOptimizedImage,
+  NgTemplateOutlet,
+} from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { environment } from '../../environments/environment';
@@ -29,6 +36,7 @@ import { TuiItem } from '@taiga-ui/cdk';
 import { MatMenuModule } from '@angular/material/menu';
 import { ThemeType } from '@cccsharonparish/angular';
 import { distinctUntilChanged, filter, Subscription } from 'rxjs';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-dashboard',
@@ -56,6 +64,8 @@ import { distinctUntilChanged, filter, Subscription } from 'rxjs';
     MatMenuModule,
     TuiAvatar,
     TuiFallbackSrcPipe,
+    NgTemplateOutlet,
+    MatSlideToggleModule,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
@@ -77,7 +87,9 @@ export class DashboardComponent extends CommonComponent {
   KEY = DASHBOARD_STRING_RESOURCE_KEY;
   ROUTE = ROUTE;
   themes: any[] = [this.KEY.LIGHT, this.KEY.DARK, this.KEY.DEVICE];
-  subscription = Subscription.EMPTY;
+  navEndSubscription = Subscription.EMPTY;
+  navStartSubscription = Subscription.EMPTY;
+
   private activatedRoute = inject(ActivatedRoute);
 
   supportedLanguages = signal<Language[]>([
@@ -97,6 +109,7 @@ export class DashboardComponent extends CommonComponent {
 
   constructor() {
     super();
+    this.onNavigationStart();
     this.onNavigationEnd();
     effect(() => {
       if (this.appStore.language().loaded) {
@@ -110,8 +123,12 @@ export class DashboardComponent extends CommonComponent {
     });
   }
 
+  closeSideDrawer() {
+    this.openSideDrawer.set(false);
+  }
+
   onNavigationEnd() {
-    this.subscription = this.router.events
+    this.navEndSubscription = this.router.events
       .pipe(
         filter((event) => event instanceof NavigationEnd),
         distinctUntilChanged()
@@ -126,16 +143,20 @@ export class DashboardComponent extends CommonComponent {
       });
   }
 
+  onNavigationStart() {
+    this.navStartSubscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationStart))
+      .subscribe(() => {
+        this.closeSideDrawer();
+      });
+  }
+
   logout() {
     this.dashboardService.logout().subscribe({
       next: () => {
         this.router.navigate([ROUTE.ROOT]);
       },
     });
-  }
-
-  onClose() {
-    this.openSideDrawer.set(false);
   }
 
   setTheme(themeType: ThemeType) {
