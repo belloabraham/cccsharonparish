e8c5cd34b190ee68ce06fac56a50eaeefe00eee8
Bello Abraham <belloabraham101@gmail.com>
2025-01-15 18:15:42 +0100

    WIP

--- apps/admin.mydailydigest.me/src/app/dashboard/new-content/content-form/content-form.component.ts
+++ apps/admin.mydailydigest.me/src/app/dashboard/new-content/content-form/content-form.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-content-form',
-  imports: [],
+  imports: [ReactiveFormsModule, MatButtonModule],
   templateUrl: './content-form.component.html',
   styleUrl: './content-form.component.scss'
 })
--- apps/admin.mydailydigest.me/src/app/dashboard/new-content/new-content.component.ts
+++ apps/admin.mydailydigest.me/src/app/dashboard/new-content/new-content.component.ts
@@ -1,20 +1,46 @@
-import { Component, effect, input, signal } from '@angular/core';
+import {
+  Component,
+  effect,
+  inject,
+  Injector,
+  input,
+  OnDestroy,
+  signal,
+} from '@angular/core';
 import { SharedModule } from '../../shared';
-import { TuiTextfield } from '@taiga-ui/core';
-import { Language } from '@cccsharonparish/mydailydigest';
+import { tuiDialog, TuiTextfield } from '@taiga-ui/core';
+import {
+  ISpiritualDailyDigest,
+  Language,
+} from '@cccsharonparish/mydailydigest';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CONTENT_STRING_RESOURCE_KEYS } from './i18n/string-res-keys';
 import { EmptyStatusComponent } from '../shared';
+import { ContentFormComponent } from './content-form/content-form.component';
+import { TuiDialogService } from '@taiga-ui/core';
+import { Subscription } from 'rxjs';
+import { PolymorpheusComponent } from '@taiga-ui/polymorpheus';
 
 @Component({
   selector: 'app-new-content',
-  imports: [SharedModule, TuiTextfield, MatButtonModule, MatIconModule, EmptyStatusComponent],
+  imports: [
+    SharedModule,
+    TuiTextfield,
+    MatButtonModule,
+    MatIconModule,
+    EmptyStatusComponent,
+    // ContentFormComponent,
+  ],
   templateUrl: './new-content.component.html',
   styleUrl: './new-content.component.scss',
 })
-export class NewContentComponent {
+export class NewContentComponent implements OnDestroy {
   readonly KEY = CONTENT_STRING_RESOURCE_KEYS;
+  private dialogService = inject(TuiDialogService);
+  private injector = inject(Injector);
+  contentFormDialogSubscription = Subscription.EMPTY;
+
   languageCode = input.required<string>({
     alias: 'languageCode',
   });
@@ -40,5 +66,22 @@ export class NewContentComponent {
       )[0].label;
       this.title.set(title);
     });
+  }
+
+  openContentDialog(selectedContent?: ISpiritualDailyDigest) {
+    this.contentFormDialogSubscription = this.dialogService
+      .open<ISpiritualDailyDigest>(
+        new PolymorpheusComponent(ContentFormComponent, this.injector),
+        {
+          data: selectedContent,
+          dismissible: false,
+          header: this.title(),
+        }
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.contentFormDialogSubscription.unsubscribe();
   }
 }
