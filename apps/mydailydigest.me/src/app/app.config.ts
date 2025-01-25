b4938fd16cac6957a659c2ebe206fffd010f489c
Bello Abraham <belloabraham101@gmail.com>
2025-01-25 11:44:07 +0100

    WIP

--- apps/mydailydigest.me/src/app/app.config.ts
+++ apps/mydailydigest.me/src/app/app.config.ts
@@ -4,7 +4,7 @@ import {
   provideAppInitializer,
   provideExperimentalZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
@@ -12,11 +12,13 @@ import { TranslocoHttpLoader } from './transloco-loader';
 import { provideTransloco } from '@jsverse/transloco';
 import { setDNSPreConnectLink } from '@cccsharonparish/mydailydigest';
 import { environment } from '../environments/environment';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withComponentInputBinding()),
+    provideAnimationsAsync(),
     provideExperimentalZonelessChangeDetection(),
 
     provideAppInitializer(() => {
