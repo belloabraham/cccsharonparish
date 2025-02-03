daa97ad7a31eea7416fe5fd79061a283909c4992
Bello Abraham <belloabraham101@gmail.com>
2025-02-03 19:32:23 +0100

    WIP

--- apps/admin.mydailydigest.me/src/app/dashboard/shared/alert-dialog/alert-dialog.component.ts
+++ apps/admin.mydailydigest.me/src/app/dashboard/shared/alert-dialog/alert-dialog.component.ts
@@ -1,18 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import type { TuiPopover } from '@taiga-ui/cdk';
-import { TuiButton, TuiDialogCloseService } from '@taiga-ui/core';
+import { TuiDialogCloseService } from '@taiga-ui/core';
 import {
   injectContext,
   PolymorpheusOutlet,
   PolymorpheusTemplate,
 } from '@taiga-ui/polymorpheus';
 import { AlertPromptOptions } from './alert-prompt-options';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   standalone: true,
   selector: 'app-alert-dialog',
-  imports: [PolymorpheusOutlet, PolymorpheusTemplate, TuiButton],
+  imports: [PolymorpheusOutlet, PolymorpheusTemplate, MatButtonModule],
   templateUrl: './alert-dialog.component.html',
   styleUrls: ['./alert-dialog.component.scss'],
   providers: [TuiDialogCloseService],
