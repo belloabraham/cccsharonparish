814d02c14e48179aadc800b11ef49ef2e140b3d8
Bello Abraham <belloabraham101@gmail.com>
2025-03-05 01:05:52 +0100

    WIP

--- apps/admin.mydailydigest.me/src/app/dashboard/approved/approved.component.ts
+++ apps/admin.mydailydigest.me/src/app/dashboard/approved/approved.component.ts
@@ -1,13 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, Signal } from '@angular/core';
 import { SharedModule } from '../../shared';
 import { APPROVED_STRING_RESOURCE_KEY } from './i18n/string-res-keys';
+import { TuiTextfield } from '@taiga-ui/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ascDescSortCompare, ColumnKeys, TABLE_MODULES } from '../shared';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { NewContentComponent } from '../new-content/new-content.component';
+import { TRANSLATE_CONTENT_TABLE_COLUMNS } from '../translate/translate-table';
+import {
+  contentsToTableUIState,
+  DEFAULT_LANG_CODE,
+  ISpiritualDailyDigestTableUIState,
+} from '@cccsharonparish/mydailydigest';
+import { tuiIsPresent } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'app-approved',
-  imports: [SharedModule],
+  imports: [
+    SharedModule,
+    TuiTextfield,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    ...TABLE_MODULES,
+  ],
   templateUrl: './approved.component.html',
   styleUrl: './approved.component.scss',
 })
-export class ApprovedComponent {
-  readonly KEY = APPROVED_STRING_RESOURCE_KEY;
+export class ApprovedComponent extends NewContentComponent {
+  APPROVED_KEY = APPROVED_STRING_RESOURCE_KEY;
+  translateTableColumns = TRANSLATE_CONTENT_TABLE_COLUMNS;
+  translateData?: Signal<ISpiritualDailyDigestTableUIState[]> = signal([]);
+
+  constructor() {
+    super();
+    this.translateData = computed(() =>
+      this._getData(
+        this.sortColumnBy(),
+        this.orderDirection(),
+        this.tablePage(),
+        this.tablePageSize()
+      ).filter(tuiIsPresent)
+    );
+  }
+
+  private _getData(
+    key: ColumnKeys,
+    direction: -1 | 1,
+    page: number,
+    size: number
+  ): ReadonlyArray<ISpiritualDailyDigestTableUIState | null> {
+    const start = page * size;
+    const end = start + size;
+    const result = [...this._getTableUIState(start, end)].sort(
+      ascDescSortCompare(key, direction)
+    );
+    return result;
+  }
+
+  _getTableUIState(start: number, end: number) {
+    const approvedContent = this.contentStore
+      .approvedContent()
+      .filter((data, index) => {
+        return index >= start && index < end;
+      });
+    const tableUIState = contentsToTableUIState(
+      approvedContent,
+      DEFAULT_LANG_CODE
+    );
+    return tableUIState;
+  }
 }
