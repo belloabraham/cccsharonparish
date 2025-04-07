4f3280d28d50bb40e9d10529fb688a4c6a01b686
Bello Abraham <belloabraham101@gmail.com>
2025-04-07 22:36:55 +0100

    WIP

--- apps/admin.mydailydigest.me/src/app/auth/i18n/string-res-keys.ts
+++ apps/admin.mydailydigest.me/src/app/auth/i18n/string-res-keys.ts
@@ -4,6 +4,7 @@ export const AUTH_STRING_RESOURCE_KEY = {
   ...SHARED_STRING_RESOURCE_KEY,
   PAGE_TITLE: 'auth_title',
   LOGIN: 'login',
+  SEND_LOGIN_LINK: 'send_login_link',
   AUTH_LOGIN_MGS: 'authorized_login_msg',
   LOGIN_LINK_SENT_TITLE: 'login_link_sent_title',
   LOGIN_LINK_SENT_MSG: 'login_link_sent_msg',
