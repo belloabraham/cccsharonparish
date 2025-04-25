3ea837d53b1153703dc8a92ad499f6dd2644990c
Bello Abraham <belloabraham101@gmail.com>
2025-04-25 18:05:01 +0100

    WIP

--- apps/mydailydigest.me/src/app/terms/terms.component.ts
+++ apps/mydailydigest.me/src/app/terms/terms.component.ts
@@ -15,5 +15,6 @@ export class TermsComponent {
   EMAIL = CONSTANT.CONTACT.EMAIL;
   ROUTE = ROUTE;
   PAGE_TRANSLATION = `https://translate.google.com/?sl=en&tl=fr&text=https%3A%2F%2Fmydailydigets.me%2Fterms&op=translate`;
-  CCC_SHARON_PARISH = CONSTANT.CCC_SHARON_PARISH;
+  CCC_SHARON_PARISH_WEBSITE = CONSTANT.CCC_SHARON_PARISH;
+  CCC_SHARON_PARISH = 'CCC Sharon Parish';
 }
