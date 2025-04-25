ee94e29a8ed1142775c7fdf2ecbad5aae6e3df38
Bello Abraham <belloabraham101@gmail.com>
2025-04-25 17:40:31 +0100

    WIP

--- apps/mydailydigest.me/src/app/terms/terms.component.ts
+++ apps/mydailydigest.me/src/app/terms/terms.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../shared';
-import { CONSTANT, environment } from '@cccsharonparish/mydailydigest';
+import { CONSTANT, environment, ROUTE } from '@cccsharonparish/mydailydigest';
 import { UpperCasePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-terms',
-  imports: [SharedModule, UpperCasePipe],
+  imports: [SharedModule, UpperCasePipe, RouterLink],
   templateUrl: './terms.component.html',
   styleUrl: './terms.component.scss',
 })
 export class TermsComponent {
   APP_NAME = environment.appName;
   EMAIL = CONSTANT.CONTACT.EMAIL;
+  ROUTE = ROUTE;
+  PAGE_TRANSLATION = `https://translate.google.com/?sl=en&tl=fr&text=https%3A%2F%2Fmydailydigets.me%2Fterms&op=translate`;
 }
