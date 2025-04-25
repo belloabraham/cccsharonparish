2a21a0ab61bfcd491fb6a075929e9a6728af5131
Bello Abraham <belloabraham101@gmail.com>
2025-04-25 16:36:42 +0100

    WIP

--- apps/mydailydigest.me/src/app/privacy/privacy.component.ts
+++ apps/mydailydigest.me/src/app/privacy/privacy.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../shared';
-import { CONSTANT, environment } from '@cccsharonparish/mydailydigest';
+import { CONSTANT, environment, ROUTE } from '@cccsharonparish/mydailydigest';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-privacy',
-  imports: [SharedModule],
+  imports: [SharedModule, RouterLink],
   templateUrl: './privacy.component.html',
   styleUrl: './privacy.component.scss',
 })
 export class PrivacyComponent {
   APP_NAME = environment.appName;
   EMAIL = CONSTANT.CONTACT.EMAIL;
+  ROUTE = ROUTE;
 }
