7521317f47ee8bd8fbf673d84012403c7e08acdd
Bello Abraham <belloabraham101@gmail.com>
2025-04-25 20:44:36 +0100

    WIP

--- apps/mydailydigest.me/src/app/home/home.component.ts
+++ apps/mydailydigest.me/src/app/home/home.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../shared';
-import { environment } from '@cccsharonparish/mydailydigest';
+import { environment, ROUTE } from '@cccsharonparish/mydailydigest';
 import { UserReviewsComponent } from "../shared/user-reviews/user-reviews.component";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [SharedModule, UserReviewsComponent],
+  imports: [SharedModule, UserReviewsComponent, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   APP_NAME = environment.appName;
+  ROUTE = ROUTE;
 }
