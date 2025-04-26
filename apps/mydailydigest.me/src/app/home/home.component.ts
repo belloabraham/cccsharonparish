0a714b8a0b279cf29350e87336429d4e182e262f
Bello Abraham <belloabraham101@gmail.com>
2025-04-26 10:51:46 +0100

    WIP

--- apps/mydailydigest.me/src/app/home/home.component.ts
+++ apps/mydailydigest.me/src/app/home/home.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../shared';
 import { environment, ROUTE } from '@cccsharonparish/mydailydigest';
-import { UserReviewsComponent } from "../shared/user-reviews/user-reviews.component";
-import { NgOptimizedImage } from '@angular/common';
+import { UserReviewsComponent } from '../shared/user-reviews/user-reviews.component';
+import { NgFor, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [SharedModule, UserReviewsComponent, NgOptimizedImage],
+  imports: [SharedModule, UserReviewsComponent, NgOptimizedImage, NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   APP_NAME = environment.appName;
   ROUTE = ROUTE;
+  cdnBaseUrl = environment.cdnBaseUrl;
+  imageUrls = [
+    `${this.cdnBaseUrl}/images/features/splash-screen.png`,
+    `${this.cdnBaseUrl}/images/features/introduction.png`,
+    `${this.cdnBaseUrl}/images/features/audio.png`,
+    `${this.cdnBaseUrl}/images/features/notification.png`,
+    `${this.cdnBaseUrl}/images/features/multilingual.png`,
+    `${this.cdnBaseUrl}/images/features/bookmarks.png`,
+  ];
 }
